48178cb1e49fbf38ed169bd95c5bfe0526df3ebc
fengwei <fengwei@yygongzi.cn>
2024-10-28 11:42:30 +0800

    fix

--- src/hooks/useTableBase.js
+++ src/hooks/useTableBase.js
@@ -220,7 +220,7 @@ const addField = async (tableId, content, successRecords, fieldTitle='签字确�
           [field.id]: [{
             "type": "url",
             "text": text,
-            "link": `${content}?recordId=${item}`
+            "link": `${content}${item}`
           }]
         }
       })
