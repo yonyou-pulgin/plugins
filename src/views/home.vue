06b31b23cd4155ec69f5f3f0cbd37999ca201253
fengwei <fengwei@yygongzi.cn>
2025-01-09 17:12:57 +0800

    build

--- src/views/home.vue
+++ src/views/home.vue
@@ -193,7 +193,8 @@ watch(() => selectFields.value.length, (val) => {
   else fieldAllChecked.value = false
 }, { deep: true })
 
-watch(() => tableInfo.value.tableId , async(val) => {
+watch(() => tableInfo.value , async(val) => {
+  if(!val) return false
   const selection = await bitable.base.getSelection();
   fromData.value.tableId = selection.tableId
   fromData.value.baseId = selection.baseId
