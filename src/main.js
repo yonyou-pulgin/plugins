43a9af69cdd37e8c42af121108aa2ab1a13d4156
fengwei <fengwei@yygongzi.cn>
2025-01-13 15:12:45 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import microApp from '@micro-zoe/micro-app'
 
-microApp.start({
-  'disable-memory-router': true, // 关闭虚拟路由系统
-  'disable-patch-request': true, // 关闭对子应用请求的拦截
-})
+// microApp.start({
+//   'disable-memory-router': true, // 关闭虚拟路由系统
+//   'disable-patch-request': true, // 关闭对子应用请求的拦截
+// })
 
 createApp(App).mount('#app')
