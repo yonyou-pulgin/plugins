79ab9909bb1a5fe91e1fb9f08c7058aeb1ae4d99
fengwei <fengwei@yygongzi.cn>
2025-01-13 14:34:13 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './pluginIndex',
+  publicPath: './',
   chainWebpack: config => { 
     config.module
       .rule('vue')
