2f1aae82f3c22d16a915eab009d0c8b7a50b4c77
fengwei <fengwei@yygongzi.cn>
2025-02-20 14:35:32 +0800

    build

--- public/config.js
+++ public/config.js
@@ -6,7 +6,7 @@ let requestCount = 0;
 // 从服务端获取最新的html代码
 async function fetchHtml() {
     // 加上时间戳避免缓存
-    return await fetch(`/version.txt?timestamp=${+new Date()}`)
+    return await fetch(`/plugin/version.txt?timestamp=${+new Date()}`)
         .then(res => res.text())
 }
 
