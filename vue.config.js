c74b35ce344412299ae978df27029756ab4690bd
fengwei <fengwei@yygongzi.cn>
2025-02-20 11:00:17 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
+// 在configureWebpack配置段内补充：
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
 
 const host = 'dev'
 const SriPlugin = require('webpack-subresource-integrity')
@@ -8,8 +12,7 @@ require('events').EventEmitter.defaultMaxListeners = 0 // 解除限制 node报�
 module.exports = defineConfig({
   transpileDependencies: false,
   lintOnSave: false,
-  publicPath: './',
-  // publicPath: process.env.NODE_ENV === 'production' ? 'https://demo.yygongzi.com/demoplugin/' : './',
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://uat.yygongzi.com/uatplugin/' : './',
   outputDir: 'dist',
   assetsDir: 'static',
   productionSourceMap: true,
@@ -54,18 +57,28 @@ module.exports = defineConfig({
   },
     // 配置 webpack 的 output
   configureWebpack: config => {
-    // if (process.env.NODE_ENV === 'production') {
-    //  // 生成时间戳
-    //   const timestamp = new Date().getTime(); // 获取当前时间的时间戳
+    if (process.env.NODE_ENV === 'production') {
+     // 生成时间戳
+      const timestamp = new Date().getTime(); // 获取当前时间的时间戳
 
-    //   // 指定 CDN 路径并添加时间戳
-    //   config.output = {
-    //     ...config.output,
-    //     filename: `[name].js?key=${timestamp}`,
-    //     chunkFilename: `[name].chunk.js?key=${timestamp}`,
-    //     publicPath: 'https://demo.yygongzi.com/demoplugin/' // CDN 路径后添加时间戳
-    //   };
-    // }
+      // 指定 CDN 路径并添加时间戳
+      config.output = {
+        ...config.output,
+        filename: `[name].js?key=new Date()`,
+        chunkFilename: `[name].chunk.js?key=new Date()`,
+        // publicPath: 'https://www.yygongzi.com/demoplugin/' // CDN 路径后添加时间戳
+      };
+      // 生产环境配置段内添加：
+      config.plugins = config.plugins.map(plugin => {
+        if (plugin instanceof MiniCssExtractPlugin) {
+          return new MiniCssExtractPlugin({
+            filename: `static/css/[name].css?key=new Date()`,
+            chunkFilename: `static/css/[name].chunk.css?key=new Date()`
+          })
+        }
+        return plugin
+      })
+    }
   },
   devServer: {
     host: '127.0.0.1',
