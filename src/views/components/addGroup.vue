30bdde8f76338adca5c534cbc1283a51d5a8ddd0
fengwei <fengwei@yygongzi.cn>
2025-02-25 11:52:39 +0800

    加入交流群

---
+++ src/views/components/addGroup.vue
@@ -0,0 +1,48 @@
+<template>
+  <yy-button class="steps-action-button yy-custom-btn-operate" @click="handleAddGroup">
+    <div class="fire-icon">
+      <svg width="15px" height="18px" viewBox="0 0 15 18" version="1.1" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink">
+        <title>正常400宽度-第一步选择数据 copy 10</title>
+        <g id="二期优化" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g id="正常400宽度-第一步选择数据-copy-10" transform="translate(-114, -691)">
+            <rect fill="#F5F6F7" x="0" y="0" width="416" height="728"></rect>
+            <rect id="Rectangle" fill="#FFFFFF" x="66" y="0" width="350" height="728"></rect>
+            <g id="Group-13" transform="translate(66, 672)">
+              <rect id="Rectangle" fill="#FFFFFF" x="0" y="0" width="350" height="56"></rect>
+              <g id="Group-7-Copy-18" transform="translate(36, 12)">
+                <rect id="Rectangle" stroke="#BBBFC4" fill="#FFFFFF" x="0.5" y="0.5" width="109" height="31" rx="4">
+                </rect>
+                <text id="🔥加入交流群" font-family="AppleColorEmoji, Apple Color Emoji" font-size="14" font-weight="normal"
+                  fill="#1F2329">
+                  <tspan x="10.3" y="24">🔥</tspan>
+                  <tspan x="29.3" y="24" font-family="PingFangSC-Regular, PingFang SC">加入交流群</tspan>
+                </text>
+              </g>
+            </g>
+          </g>
+        </g>
+      </svg>
+
+      加入交流群
+    </div>
+  </yy-button>
+</template>
+
+<script setup>
+import yyButton from '@/antDesignComponents/yyButton/yy-button.vue';
+
+const handleAddGroup = () => {
+  window.open('https://go.larkoffice.com/join-chat/22cs5666-9280-479f-9afc-c6a2792a8e41')
+}
+</script>
+
+<style lang="scss" scoped>
+.fire-icon{
+  display: flex;
+  align-items: center;
+  svg{
+    margin-top: -5px;
+  }
+}
+</style>
--- src/views/createForm.vue
+++ src/views/createForm.vue
@@ -9,6 +9,7 @@
     </div>
     <yySteps class="plugins-steps" :class="{ 'plugins-steps-isVerifyIdentity': isVerifyIdentityCheck }" :steps="stepList"
       :indes="current" @next="handleNext" @prev="handlePrev">
+      <addGroup />
       <yy-button v-if="current && current < stepList.length - 1 && current != stepList.length - 1"
         class="steps-action-button yy-custom-btn-operate" @click="handlePrev">上一步</yy-button>
       <yy-button v-if="current == 0 && current != stepList.length - 1" class="steps-action-button yy-custom-btn-operate"
@@ -54,6 +55,7 @@ import useConfirmInfo from '@/hooks/useConfirmInfo'
 import useTableBase from '@/hooks/useTableBase.js';
 import { detail } from './data';
 import jslog from 'jsLog';
+import addGroup from './components/addGroup.vue';
 
 const { toClipboard } = useClipboard()
 const { setTableInfo, tableInfo, tenantKey, addField, userId, fieldList, tableData, tableName, addImgField, getCellUrlResult, checkHasAttachment,
